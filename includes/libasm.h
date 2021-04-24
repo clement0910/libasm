7333640b2b3210891b13835218b20ee2d3e84d4b
Clément <csapt2001@gmail.com>
2021-04-24 17:23:52 +0200

    add header :label:

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,7 +6,7 @@
 /*   By: csapt <csapt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 13:37:23 by csapt             #+#    #+#             */
-/*   Updated: 2021/04/24 15:05:54 by csapt            ###   ########lyon.fr   */
+/*   Updated: 2021/04/24 17:08:43 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 # define LIBASM_H
 
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/errno.h>
 
 size_t	ft_strlen(const char *src);
+char	*ft_strcpy(char * dst, const char * src);
+int		ft_strcmp(const char *s1, const char *s2);
+
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t ft_read(int fildes, void *buf, size_t nbyte);
+char	*ft_strdup(const char *s1);
 #endif
